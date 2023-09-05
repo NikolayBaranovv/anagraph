3868bc06f2b788e33bd9cae36a3e31cb98fb6ee0
Ilya Skriblovsky <ilyaskriblovsky@gmail.com>
2023-09-05 22:32:35 +0300

    building with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,52 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import replace from "rollup-plugin-replace";
 
 const packageJson = require("./package.json");
 
 export default [
+    // {
+    //     input: "src/lib/index.ts",
+    //     output: [
+    //         {
+    //             file: packageJson.main,
+    //             format: "cjs",
+    //             sourcemap: true,
+    //         },
+    //         {
+    //             file: packageJson.module,
+    //             format: "esm",
+    //             sourcemap: true,
+    //         },
+    //     ],
+    //     plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" }) /*terser()*/],
+    //     external: ["react", "react-dom"],
+    // },
+    // {
+    //     input: "src/lib/index.ts",
+    //     output: [{ file: "dist/anagraph.d.ts", format: "es" }],
+    //     plugins: [dts.default()],
+    // },
+
     {
-        input: "src/lib/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
+        input: "src/index.tsx",
+        output: {
+            dir: "dist",
+            sourcemap: true,
+        },
+        plugins: [
+            peerDepsExternal(),
+            replace({
+                "process.env.NODE_ENV": '"production"',
+            }),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+            serve({ contentBase: "dist", open: true }),
+            livereload("dist"),
         ],
-        plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" }) /*terser()*/],
-        external: ["react", "react-dom"],
-    },
-    {
-        input: "src/lib/index.ts",
-        output: [{ file: "dist/anagraph.d.ts", format: "es" }],
-        plugins: [dts.default()],
     },
 ];
